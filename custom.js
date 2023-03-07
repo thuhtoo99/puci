8ecf13dc86541040393fa55567bf0ab9f63be9a8
thuhtoo99 <66209926+thuhtoo99@users.noreply.github.com>
2023-03-07 09:35:04 +0630

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,54 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+$('.generate-iframe').click(function(){
+
+	
+
+var code = $('#iframe-code').val();
+var url = $('#iframe-url').val();
+
+
+$('#iframe-output-area1').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'.mp4');
+$('#iframe-output-area2').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_hd.mp4');
+$('#iframe-output-area3').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_dvd.mp4');
+$('#iframe-output-area4').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_std.mp4');
+$('#iframe-output-area5').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_hd.original.jpg');
+$('#iframe-output-area6').val('https://videopress.com/v/'+ code +'/');
+	});
+});
+
+// Copy //
+
+
+function copy1() {
+  let textarea = document.getElementById("iframe-output-area1");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy2() {
+  let textarea = document.getElementById("iframe-output-area2");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy3() {
+  let textarea = document.getElementById("iframe-output-area3");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy4() {
+  let textarea = document.getElementById("iframe-output-area4");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy5() {
+  let textarea = document.getElementById("iframe-output-area5");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy6() {
+  let textarea = document.getElementById("iframe-output-area6");
+  textarea.select();
+  document.execCommand("copy");
+}
+
+
